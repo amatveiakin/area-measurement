d5983142f5cb1c4850f24b8a95f7d9cb1792435f
Andrey Matveyakin <a.matveyakin@gmail.com>
2012-11-04 22:37:40 +0400

    Add toolbar and draw etalon

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include <QFileDialog>
 #include <QImageReader>
+#include <QInputDialog>
 #include <QMessageBox>
 #include <QPainter>
 #include <QPaintEvent>
@@ -13,6 +14,18 @@
 
 const QString appName = "AreaMeasurement";
 
+const QString linearUnit = QString::fromUtf8("м");
+const QString squareUnit = linearUnit + QString::fromUtf8("²");
+
+enum Mode
+{
+  SET_ETALON,
+  MEASURE_SEGMENT_LENGTH,
+  MEASURE_POLYLINE_LENGTH,
+  MEASURE_RECTANGLE_AREA,
+  MEASURE_POLYGON_AREA
+};
+
 class CanvasWidget : public QWidget
 {
 public:
@@ -21,6 +34,9 @@ public:
     image_(image)
   {
     setFixedSize(image_->size());
+    setMouseTracking(true);
+    mode_ = SET_ETALON;
+    nPointsSet_ = 0;
   }
 
   ~CanvasWidget()
@@ -32,10 +48,61 @@ public:
   {
     QPainter painter(this);
     painter.drawPixmap(event->rect().topLeft() , *image_, event->rect());
+    switch (mode_) {
+      case SET_ETALON:
+        if (nPointsSet_ == 1) {
+          painter.setPen(Qt::gray);
+          painter.drawLine(etalon_.p1(), pointUnderMouse_);
+        }
+        else if (nPointsSet_ == 2) {
+          painter.setPen(Qt::blue);
+          painter.drawLine(etalon_);
+        }
+      case MEASURE_SEGMENT_LENGTH:
+      case MEASURE_POLYLINE_LENGTH:
+      case MEASURE_RECTANGLE_AREA:
+      case MEASURE_POLYGON_AREA:
+        break;
+    }
+  }
+
+  virtual void mousePressEvent(QMouseEvent* event)
+  {
+    if (event->buttons() != Qt::LeftButton)
+      return;
+    switch (mode_) {
+      case SET_ETALON:
+        if (nPointsSet_ >= 2)
+          nPointsSet_ = 0;
+        if (nPointsSet_ == 0)
+          etalon_.setP1(event->pos());
+        else if (nPointsSet_ == 1) {
+          etalon_.setP2(event->pos());
+          QInputDialog::getInt(this, appName, QString::fromUtf8("Укажите длину эталона (%1): ").arg(linearUnit), 0, 0);
+        }
+        nPointsSet_++;
+        break;
+      case MEASURE_SEGMENT_LENGTH:
+      case MEASURE_POLYLINE_LENGTH:
+      case MEASURE_RECTANGLE_AREA:
+      case MEASURE_POLYGON_AREA:
+        break;
+    }
+  }
+
+  virtual void mouseMoveEvent(QMouseEvent* event)
+  {
+    pointUnderMouse_ = event->pos();
+    update();
   }
 
 private:
   const QPixmap* image_;
+  Mode mode_;
+  int nPointsSet_;
+  QLine etalon_;
+  QPoint pointUnderMouse_;
+  double etalonLength_;
 };
 
 
@@ -45,6 +112,21 @@ MainWindow::MainWindow(QWidget* parent) :
 {
   ui->setupUi(this);
   setAttribute(Qt::WA_QuitOnClose);
+  setWindowTitle(appName);
+
+  QActionGroup* modeActionGroup = new QActionGroup(this);
+  QAction* setEtalonAction             = new QAction(QString::fromUtf8("Задание эталона"),                    modeActionGroup);
+  QAction* measureSegmentLengthAction  = new QAction(QString::fromUtf8("Измерение длин отрезков"),            modeActionGroup);
+  QAction* measurePolylineLengthAction = new QAction(QString::fromUtf8("Измерение длин кривых"),              modeActionGroup);
+  QAction* measureRectangleAreaAction  = new QAction(QString::fromUtf8("Измерение площадей прямоугольников"), modeActionGroup);
+  QAction* measurePolygonAreaAction    = new QAction(QString::fromUtf8("Измерение площадей многоугольников"), modeActionGroup);
+  setEtalonAction            ->setCheckable(true);
+  measureSegmentLengthAction ->setCheckable(true);
+  measurePolylineLengthAction->setCheckable(true);
+  measureRectangleAreaAction ->setCheckable(true);
+  measurePolygonAreaAction   ->setCheckable(true);
+  setEtalonAction->setChecked(true);
+  ui->mainToolBar->addActions(modeActionGroup->actions());
 
   QList<QByteArray> supportedFormatsList = QImageReader::supportedImageFormats();
   QString supportedFormatsString;
@@ -52,13 +134,13 @@ MainWindow::MainWindow(QWidget* parent) :
      supportedFormatsString += "*." + QString(format).toLower() + " ";
 
   QString imageFile = QFileDialog::getOpenFileName (this, QString::fromUtf8("Укажите путь к изображению — ") + appName,
-                                                    QString(), QString::fromUtf8("Все изображения (") + supportedFormatsString + ")");
+                                                    QString(), QString::fromUtf8("Все изображения (%1)").arg(supportedFormatsString));
   if (imageFile.isEmpty ())
     close();
 
   QPixmap* image = new QPixmap;
   if (!image->load(imageFile)) {
-    QMessageBox::warning(this, appName, QString::fromUtf8("Не могу открыть изображение \"") + imageFile + "\".");
+    QMessageBox::warning(this, appName, QString::fromUtf8("Не могу открыть изображение \"%1\".").arg(imageFile));
     delete image;
     close();
   }
